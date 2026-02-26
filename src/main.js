014ed19cf290dcf5f4207775740ad4df6c5aa6e0
samira-2002-code <samiraassemlali2@gmail.com>
2026-02-26 14:48:13 +0000

    importation of function getRecipes and displayRecipes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
- 
+import { getRecipes } from "./Api/recipeProvider.js";
+
+getRecipes();
+
+import { displayRecipes } from "./ui/render.js";
+
+async function init() {
+  const recipes = await getRecipes();
+  displayRecipes(recipes);
+}
+
+init(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
