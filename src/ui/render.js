c561abe1c9595c55e1b9972c8241662da385d42e
samira-2002-code <samiraassemlali2@gmail.com>
2026-02-26 15:26:20 +0000

    function getBadgclor is addd

--- src/ui/render.js
+++ src/ui/render.js
@@ -5,6 +5,9 @@ export function displayRecipes(recipes) {
   const container = document.querySelector(".recipes");
 
   recipes.forEach(recipe => {
+
+    const badgeColor = getBadgeColor(recipe.caloriesPerServing);
+
     const card = document.createElement("div");
     card.classList.add("card");
 
@@ -12,8 +15,18 @@ export function displayRecipes(recipes) {
       <img src="${recipe.image}" width="150"/>
       <h3>${recipe.name}</h3>
       <p>${recipe.prepTimeMinutes} min</p>
+
+      <span class="badge ${badgeColor}">
+        ${recipe.caloriesPerServing} kcal
+      </span>
     `;
 
     container.appendChild(card);
   });
+}
+
+function getBadgeColor(calories) {
+  if (calories < 400) return "green";
+  if (calories <= 800) return "orange";
+  return "red";
 }
