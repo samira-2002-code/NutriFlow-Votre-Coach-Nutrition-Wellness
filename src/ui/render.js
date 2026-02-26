7cb0692c80ddd867b278dd1af5fb444fc702368e
samira-2002-code <samiraassemlali2@gmail.com>
2026-02-26 14:47:09 +0000

    addedfunction displayRecipes

--- src/ui/render.js
+++ src/ui/render.js
@@ -1,1 +1,19 @@
- 
+export function displayRecipes(recipes) {
+  const app = document.getElementById("app");
+
+  app.innerHTML = "<div class='recipes'></div>";
+  const container = document.querySelector(".recipes");
+
+  recipes.forEach(recipe => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+      <img src="${recipe.image}" width="150"/>
+      <h3>${recipe.name}</h3>
+      <p>${recipe.prepTimeMinutes} min</p>
+    `;
+
+    container.appendChild(card);
+  });
+}
