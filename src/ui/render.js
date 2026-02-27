a141b06b2e3ce53fb5ac57bc4afdc33f09fc42cd
samira-2002-code <samiraassemlali2@gmail.com>
2026-02-27 10:39:18 +0000

    feat: add recipe detail modal view

--- src/ui/render.js
+++ src/ui/render.js
@@ -1,17 +1,16 @@
 export function displayRecipes(recipes) {
-  const app = document.getElementById("app");
+    const app = document.getElementById("app");
 
-  app.innerHTML = "<div class='recipes'></div>";
-  const container = document.querySelector(".recipes");
+    app.innerHTML = "<div class='recipes'></div>";
+    const container = document.querySelector(".recipes");
 
-  recipes.forEach(recipe => {
+    recipes.forEach((recipe) => {
+        const badgeColor = getBadgeColor(recipe.caloriesPerServing);
 
-    const badgeColor = getBadgeColor(recipe.caloriesPerServing);
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    card.innerHTML = `
+        card.innerHTML = `
       <img src="${recipe.image}" width="150"/>
       <h3>${recipe.name}</h3>
       <p>${recipe.prepTimeMinutes} min</p>
@@ -21,12 +20,33 @@ export function displayRecipes(recipes) {
       </span>
     `;
 
-    container.appendChild(card);
-  });
+        container.appendChild(card);
+
+        card.addEventListener("click", () => {
+            openModal(recipe);
+        });
+    });
 }
 
 function getBadgeColor(calories) {
-  if (calories < 400) return "green";
-  if (calories <= 800) return "orange";
-  return "red";
+    if (calories < 400) return "green";
+    if (calories <= 800) return "orange";
+    return "red";
+}
+function openModal(recipe) {
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.innerHTML = `<div class="modal-content">
+  <h2<${recipe.name}</h2>
+  <h4>Ingredients :</h4>
+  <ul> ${recipoe.Ingredients.map((item) => `<li>${item}</li>.join("")`)}
+   </ul>
+     <h4>Instructions :</h4>
+     <p>${recipe.Instructions}     </p>
+     <button class="close-btn">Fermer</button>
+     </div>`;
+     document.body.appendChild(modal);
+     modal.querySelector(".close-btn").addEventListener("click",()=>{
+      modal.remove();
+     });
 }
