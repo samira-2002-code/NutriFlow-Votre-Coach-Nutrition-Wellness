595fac5aeadd10e1364f22187e964c7268254e1a
samira-2002-code <samiraassemlali2@gmail.com>
2026-02-27 09:50:24 +0000

    importation of showloader in main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
-import { getRecipes } from "./Api/recipeProvider.js";
-
-getRecipes();
-
+import { getRecipes } from "./api/recipeProvider.js";
 import { displayRecipes } from "./ui/render.js";
+import { showLoader } from "./ui/loader.js";
 
 async function init() {
+
+ 
+  showLoader();
+
+ 
   const recipes = await getRecipes();
+
+  
   displayRecipes(recipes);
 }
 
-init(); 
-
-
-
+init();
 
 
 
